1b26e6bdad03c812b779eeb1a548f1dceb1f1151
Giacomo D'angelo <118721242+jackydangelo@users.noreply.github.com>
2026-05-10 02:13:23 +0200

    Add configuration for gamification news feed

---
+++ config.py
@@ -0,0 +1,18 @@
+from zoneinfo import ZoneInfo
+
+SOURCE_RSS = [
+    "https://news.google.com/rss/search?q=gamification",
+    "https://news.google.com/rss/search?q=gamification&hl=it&gl=IT&ceid=IT:it"
+]
+
+DAYS_LIMIT = 30
+
+TIMEZONE = ZoneInfo("Europe/Rome")
+
+PAGE_TITLE = "Gamification News"
+
+FOOTER_TEXT = (
+    'Feed per temi di gamification. '
+    'Parte del progetto "Questa è gamification!" '
+    "di Giacomo D'angelo."
+)
